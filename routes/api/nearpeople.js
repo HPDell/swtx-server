ef156b9a7084507d3828d72671bbccab000b8b51
HPDell <HuYG0180110559@outlook.com>
2017-07-26 18:17:43 +0800

    临时提交

---
+++ routes/api/nearpeople.js
@@ -0,0 +1,8 @@
+/// <reference path="../../typings/index.d.ts" />
+var express = require('express');
+var mysql = require('mysql');
+var router = express.Router();
+
+router.get('/nearpeople', function (req, res, next) {
+    
+})
---
+++ routes/api/nearpeople.ts
@@ -0,0 +1,8 @@
+// /// <reference path="../../typings/index.d.ts" />
+
+import * as express from "express";
+import * as mysql from "mysql";
+
+module NearPeople {
+    
+}
