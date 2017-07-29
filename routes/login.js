a763f40250fc015826e2a168e6aced71838cab91
HPDell <HuYG0180110559@outlook.com>
2017-07-29 14:21:32 +0800

    完善登录

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,43 @@
 exports.__esModule = true;
 var mysql = require("mysql");
 function login(req, res, next) {
-    var data = req.body;
-    var queryString = "";
+    if (req.body.UserName) {
+        var data = req.body;
+        doQuery(data, req, res);
+    }
+    else {
+        var body = "";
+        req.on('data', function (chunk) {
+            body += chunk;
+        });
+        req.on('end', function () {
+            try {
+                var data = JSON.parse(body);
+                doQuery(data, req, res);
+            }
+            catch (error) {
+                res.json({
+                    result: false
+                });
+            }
+        });
+    }
+}
+exports.login = login;
+/**
+ * 执行查询操作
+ * @param data 请求所发送的数据
+ * @param req 请求
+ * @param res 响应
+ */
+function doQuery(data, req, res) {
+    var queryString = "SELECT\
+            Users.UserName,\
+            Users.UserPassword\
+        FROM\
+            Users\
+        WHERE\
+        Users.UserName = '" + data.UserName + "'";
     // 连接数据库
     var connection = mysql.createConnection({
         host: "139.129.166.245",
@@ -16,6 +51,7 @@ function login(req, res, next) {
     // 查询
     connection.query(queryString, function (err, results) {
         if (err) {
+            console.log(err.message);
             res.json({
                 result: false
             });
@@ -28,7 +64,7 @@ function login(req, res, next) {
                         result: true
                     });
                     // TODO 数据库记录登录IP
-                    console.log("Request IP:" + req.ip);
+                    updateIp(data.UserName, req.ip);
                 }
                 else {
                     res.json({
@@ -43,5 +79,26 @@ function login(req, res, next) {
             }
         }
     });
+    connection.end();
 }
-exports.login = login;
+/**
+ * 更新地址
+ * @param ip IP地址
+ */
+function updateIp(user, ip) {
+    var connection = mysql.createConnection({
+        host: "139.129.166.245",
+        port: 3306,
+        user: "admin",
+        password: "admin",
+        database: "swtx"
+    });
+    connection.connect();
+    // 更新IP
+    var queryString = "UPDATE Users SET Users.UserLastIP = '" + ip + "' WHERE Users.UserName = '" + user + "'";
+    connection.query(queryString, function (err, results) {
+        if (err) {
+            console.log(err.message);
+        }
+    });
+}
--- routes/login.ts
+++ routes/login.ts
@@ -12,8 +12,43 @@ interface ILoginQueryResult {
 }
 
 export function login(req:express.Request, res:express.Response, next:express.NextFunction) {
-    var data:ILoginParam = req.body;
-    var queryString:string = ``;
+    if (req.body.UserName) {
+        var data:ILoginParam = req.body;
+        doQuery(data, req, res);
+    } else {
+        var body:string = "";
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+        req.on('end', () => {
+            try {
+                var data:ILoginParam = JSON.parse(body);
+                doQuery(data, req, res);
+            } catch (error) {
+                res.json({
+                    result: false
+                })
+            }
+        })
+    }
+}
+
+/**
+ * 执行查询操作
+ * @param data 请求所发送的数据
+ * @param req 请求
+ * @param res 响应
+ */
+function doQuery(data:ILoginParam, req:express.Request, res:express.Response) {
+    var queryString:string = 
+        "SELECT\
+            Users.UserName,\
+            Users.UserPassword\
+        FROM\
+            Users\
+        WHERE\
+        Users.UserName = '" + data.UserName + "'";
+
     // 连接数据库
     var connection = mysql.createConnection({
         host: "139.129.166.245",
@@ -26,6 +61,7 @@ export function login(req:express.Request, res:express.Response, next:express.Ne
     // 查询
     connection.query(queryString, (err:mysql.IError, results:Array<ILoginQueryResult>) => {
         if (err) {
+            console.log(err.message);
             res.json({
                 result: false
             });
@@ -37,7 +73,7 @@ export function login(req:express.Request, res:express.Response, next:express.Ne
                         result: true
                     });
                     // TODO 数据库记录登录IP
-                    console.log("Request IP:" + req.ip);
+                    updateIp(data.UserName, req.ip);
                 } else {
                     res.json({
                         result: false
@@ -50,4 +86,28 @@ export function login(req:express.Request, res:express.Response, next:express.Ne
             }
         }
     });
+
+    connection.end();
+}
+
+/**
+ * 更新地址
+ * @param ip IP地址
+ */
+function updateIp(user:string, ip:string) {
+    var connection = mysql.createConnection({
+        host: "139.129.166.245",
+        port: 3306,
+        user: "admin",
+        password: "admin",
+        database: "swtx"
+    });
+    connection.connect();
+    // 更新IP
+    var queryString:string = "UPDATE Users SET Users.UserLastIP = '" + ip + "' WHERE Users.UserName = '" + user + "'";
+    connection.query(queryString, (err:mysql.IError, results:Array<ILoginQueryResult>) => {
+        if (err) {
+            console.log(err.message);
+        }
+    })
 }
